c9a691ba2fa3dbda34aa68809218191e8cdb153a
dima kiselev <d.s.matroskin@gmail.com>
2022-09-17 23:32:03 +0300

    add react

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Counter from './components/Counter';
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>
+);
