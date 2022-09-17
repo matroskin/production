59bb4145f7e15b9da5881915a0dd5da627daa15b
dima kiselev <d.s.matroskin@gmail.com>
2022-09-17 21:19:50 +0300

    add decomposition webpack config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,14 @@
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { BuildOptions } from './types/config';
+
+export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstance[] {
+  const { paths } = options;
+
+  return [
+    new HtmlWebpackPlugin({
+      template: paths.html,
+    }),
+    new webpack.ProgressPlugin(),
+  ];
+}
