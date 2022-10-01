5a4a8910cee1a6a8a075ec22bdcd31a65911ceaf
dima kiselev <d.s.matroskin@gmail.com>
2022-10-01 20:47:53 +0300

    add applink component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,8 +10,8 @@ export const Navbar = ({className}: NavbarProps) => {
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
       <div className={cls.links}>
-        <Link to={'/'}>Главная</Link>
-        <Link to={'/about'}>О компании</Link>
+        <AppLink to={'/'}>Главная</AppLink>
+        <AppLink to={'/about'}>О компании</AppLink>
       </div>
     </div>
   );
