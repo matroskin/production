15ec371b45293e23abb47913a23f23a3a78478c1
dima kiselev <d.s.matroskin@gmail.com>
2022-10-08 19:23:53 +0300

    add theme switcher component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,10 @@
 import { Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { AppRouter } from './providers/router';
 import { useTheme } from './providers/ThemeProvider';
 import './styles/index.scss';
-
-const Component = () => {
-  const { t, i18n } = useTranslation();
-
-  const toggle = () => {
-    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
-  }
-
-  return (
-    <div>
-      <button onClick={toggle}>{t('Перевод')}</button>
-    </div>
-  );
-};
 
 const App = () => {
   const { theme } = useTheme();
@@ -28,7 +13,6 @@ const App = () => {
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback=''>
         <Navbar />
-        <Component />
         <div className='page-wrapper'>
           <Sidebar />
           <AppRouter />
