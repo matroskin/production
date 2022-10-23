42998d5d08bcbc80c2916c73dbbae60d17a903f4
dima kiselev <d.s.matroskin@gmail.com>
2022-10-23 21:41:42 +0300

    add loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 function AppRouter() {
   return (
@@ -11,7 +12,7 @@ function AppRouter() {
           path={path}
           element={(
             <div className='page-content'>
-              <Suspense fallback={<div>Loading...</div>}>
+              <Suspense fallback={<PageLoader />}>
                 { element }
               </Suspense>
             </div>
